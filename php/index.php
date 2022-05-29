72be21ce41910d9a74e54088381518ec437b31c0
Sebastián Grisales <96491228+JsGrisales@users.noreply.github.com>
2022-05-29 17:40:56 -0500

    Add PHP

---
+++ php/index.php
@@ -0,0 +1,8 @@
+<?php
+$nombre= $_REQUEST ["nombre"];
+$email= $_REQUEST ["email"];
+$tel= $_REQUEST ["tel"];
+$comentario= $_REQUEST ["comentario"];
+echo $nombre." || ".$email. " || " .$comentario. " || " .$tel;
+
+?>
